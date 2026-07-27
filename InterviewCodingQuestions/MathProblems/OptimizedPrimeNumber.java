f6b55075b22320137d591ccf0ea7daa6591d90fa
rati <patilratikant8@gmail.com>
2026-07-27 20:55:18 +0530

    JavaBasics

--- InterviewCodingQuestions/MathProblems/OptimizedPrimeNumber.java
+++ InterviewCodingQuestions/MathProblems/OptimizedPrimeNumber.java
@@ -1,6 +1,22 @@
 package MathProblems;
 
 public class OptimizedPrimeNumber {
+    public static void main(String [] args){
+        int number = 120;
+
+        if(number<=1){
+            System.out.println("not a prime");
+            return;
+        }
+
+        for(int i = 2 ; i*i<=number; i++){
+            if(number%i==0){
+                System.out.println("not a prime");
+                return;
+            }
+        }
+        System.out.println("prime");
+    }
 
 
 }
