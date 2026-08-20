29e72710b0000901fe6e6696b0967a51bc0bf194
rati <patilratikant8@gmail.com>
2026-08-20 09:45:28 +0530

    hello

---
+++ core-java/src/com/ratipatil/corejava/Collections/practice/ArrayList.java
@@ -0,0 +1,19 @@
+package com.ratipatil.corejava.Collections.practice;
+
+public class ArrayList<I extends Number> {
+    
+    public static void main(String [] args){
+        
+        ArrayList<Integer> newArray = new ArrayList<>();
+        
+        newArray.add(90);
+        
+
+
+    }
+
+    private void add(I i) {
+    }
+
+
+}
