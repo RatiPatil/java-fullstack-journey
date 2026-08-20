d9a72a65ed78a7169ff8a9c3008872f149f919ee
rati <patilratikant8@gmail.com>
2026-08-20 08:57:24 +0530

    Collections

---
+++ core-java/src/com/ratipatil/corejava/Collections/Students.java
@@ -0,0 +1,25 @@
+package com.ratipatil.corejava.Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students {
+
+    public static void main(String [] args) {
+
+        List<String> newlist = new  ArrayList<>();
+
+        newlist.add("Ratikant");
+        newlist.add("Patil");
+        newlist.add("Hello");
+
+
+        System.out.println(newlist);
+
+    }
+
+
+
+
+
+}
