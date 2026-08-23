892ea15a4f3b288f804ccdf0dcd2e71a7132454e
rati <patilratikant8@gmail.com>
2026-08-23 15:07:55 +0530

    ...

--- core-java/src/com/ratipatil/corejava/Collections/practice/StudentRegistrySystem.java
+++ core-java/src/com/ratipatil/corejava/Collections/practice/StudentRegistrySystem.java
@@ -1,4 +1,24 @@
 package com.ratipatil.corejava.Collections.practice;
 
+
+
+class Student{
+    public int id ;
+
+    public String name ;
+
+    public double gpa ;
+
+    Student()
+
+}
 public class StudentRegistrySystem {
+    public static void main(String [] args){
+
+
+
+
+
+
+    }
 }
