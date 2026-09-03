71fe4523434bdff891e3009d2e6c948b628a8838
rati <patilratikant8@gmail.com>
2026-09-03 16:30:53 +0530

    asdfghjkjhngbfSAAAvcdjmijijxzasxcdfvgb

--- Methods/DuplicateNumber.java
+++ Methods/DuplicateNumber.java
@@ -1,17 +1,14 @@
+import java.util.*;
+
 public class DuplicateNumber {
+    public static void main(String [] data){
+        int [] numbers = {1,2,3,4,5,6,7,5};
 
-    public static void main(String[] args) {
-
-        int[] numbers = {1, 2, 3, 4, 2, 5, 3};
-
-        System.out.println("Duplicate Numbers:");
-
-        for (int i = 0; i < numbers.length; i++) {
-
-            for (int j = i + 1; j < numbers.length; j++) {
-
-                if (numbers[i] == numbers[j]) {
-                    System.out.println(numbers[i]);
+        for(int i = 0 ; i < numbers.length ; i++){
+            // Start j at i + 1 to avoid comparing a number with itself
+            for(int j = i + 1 ; j < numbers.length; j++){
+                if(numbers[i] == numbers[j]){
+                    System.out.println("Duplicate found: " + numbers[i]);
                     break;
                 }
             }
