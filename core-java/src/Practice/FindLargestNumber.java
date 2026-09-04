8cae7209300ed7e9cc589d5aaeeeceeaa956d758
rati <patilratikant8@gmail.com>
2026-09-04 11:32:02 +0530

    asdfghjkjhngbfSAAAvcdjmijijxzasxcdfvgb

---
+++ core-java/src/Practice/FindLargestNumber.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class FindLargestNumber {
+
+    public static void main(String[] args){
+        int [] numbers = {12,3,45,32,4,67};
+
+
+        int largest = numbers[1];
+
+        for(int i = 0 ; i<numbers.length; i++){
+            if(numbers[i] > largest){
+                System.out.println("The largest Number is : " + largest + " And the index is the  " + )
+            }
+        }
+    }
+}
